b7357cd87d082e889efa3b9afe3090f20cf4824f
ciky <564140599@qq.com>
2025-06-20 12:02:09 +0800

    修改活动程度枚举
    - 新增活动系数

--- src/main/java/cloud/ciky/enums/ActivityLevelEnum.java
+++ src/main/java/cloud/ciky/enums/ActivityLevelEnum.java
@@ -12,14 +12,15 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ActivityLevelEnum {
 
-    SEDENTARY("0", "久坐不活动"),
-    LIGHT("1", "轻度活动"),
-    MODERATE("2", "中度活动"),
-    HIGH("3", "重度活动"),
-    EXTREME("4", "极重度活动");
+    SEDENTARY("0", "久坐不活动", 1.2),
+    LIGHT("1", "轻度活动", 1.375),
+    MODERATE("2", "中度活动", 1.55),
+    HIGH("3", "重度活动", 1.725),
+    EXTREME("4", "极重度活动", 1.9);
 
     private final String code;
     private final String desc;
+    private final double activeFactor;
 
     /**
      * 根据code获取枚举
